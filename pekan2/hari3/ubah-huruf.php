852182cac467bd420494f8eb26762edb313f3fb4
gsihaj5 <gsihaj5@gmail.com>
2020-07-30 00:08:23 +0700

    C7P2D3 DONE

---
+++ pekan2/hari3/ubah-huruf.php
@@ -0,0 +1,16 @@
+<?php
+function ubah_huruf($string){
+//kode di sini
+    for($i = 0; $i < strlen($string); $i ++)
+        echo chr(ord($string[$i]) + 1);
+    echo "<br>";
+}
+
+// TEST CASES
+echo ubah_huruf('wow'); // xpx
+echo ubah_huruf('developer'); // efwfmpqfs
+echo ubah_huruf('laravel'); // mbsbwfm
+echo ubah_huruf('keren'); // lfsfo
+echo ubah_huruf('semangat'); // tfnbohbu
+
+?>
